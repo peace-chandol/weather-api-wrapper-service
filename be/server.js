8f3d026a624a06ba00a5ea6eaaa988114bd744f7
Peace Chandol <peace.hyperion@gmail.com>
2024-08-01 01:30:21 +0700

    cors

--- be/server.js
+++ be/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const connectDB = require('./config/dbConn')
 const PORT = process.env.PORT || 3000
 
 connectDB()
+app.use(cors())
 
 app.use('/', require('./routes/weatherRoute')) 
 
