c71d8a864b1a4edd5a9ae9a402dfd2ee574cb98e
Peace Chandol <peace.hyperion@gmail.com>
2024-08-02 02:50:24 +0700

    finish

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+
+    // FORM COMPONENT
     const [citySearch, setCitySearch] = useState<string>()
     const [data, setData] = useState<any>()
     const [errMsg, setErrMsg] = useState<string>()
@@ -17,13 +19,13 @@ function App() {
         const url = `http://localhost:3000/weather/${cityParam}`
         try {
             const response = await axios.get(url)
-            console.log('this is log data', response.data)
+            console.log('this is data', response.data)
             setData(response.data)
             return response.data
         } catch (err: any) {
             console.error(err)
             if (err.response.status === 400) {
-                setErrMsg('Invalid City or Country')
+                setErrMsg('*Invalid City or Country')
             }
             else {
                 setErrMsg('Error')
@@ -42,23 +44,64 @@ function App() {
         inputCity.value = ''
     }
 
-    return (
-        <>
-            <h1>Weather Forecast</h1>
-            <form>
-                <label>City :</label>
-                <input type='text' id='input-city' />
-                <button type='submit' id='btn-search'>Search</button>
-            </form>
+    // DISPLAY COMPONENT
+    function displayHour(time: string) {
+        return time.split(':')[0]
+    }
 
-            <div>
-                <h1>For show Weather!</h1>
-                {errMsg}
-                <h2>{data?.city}</h2>
-                <p>{data?.currentTemp}</p>
-                {/* DO: display data */}
+    function displayTemp(temp: number) {
+        if (!temp) return
+        return `${Math.round(temp).toString()}°`
+    }
+
+    function showMoreDetails(index: number) {
+        const hoursContainer = document.getElementsByClassName('hours-container')[index]
+        hoursContainer.classList.toggle('hide')
+    }
+
+    return (
+        <div className='container'>
+            <div className='search-form-container'>
+                <h1>Weather Forecast</h1>
+                <form className='form-container'>
+                    <label>City | Country</label>
+                    <input type='text' id='input-city' />
+                    <button type='submit' id='btn-search'>Search</button>
+                </form>
             </div>
-        </>
+
+            <div className='error-msg-container'>
+                <p>{errMsg}</p>
+            </div>
+
+            <div className='data-display-container'>
+                <h4>{data?.city}</h4>
+                <h2>{displayTemp(data?.currentTemp)}</h2>
+                <div className='frecast-display-container'>
+                    {data?.days?.map((day: any, index: number) => (
+                        <div key={index} className='days-container'>
+                            <div className='days-data-only-container'>
+                                <p>{day?.datetime}</p>
+                                <img src={`src/assets/icon/${day.icon}.svg`} alt={day.icon} className='icon-display' />
+                                <p>{`L: ${displayTemp(day?.tempmin)}`}</p>
+                                <p>{`Temp ${displayTemp(day?.temp)}`}</p>
+                                <p>{`H: ${displayTemp(day?.tempmax)}`}</p>
+                                <button onClick={() => showMoreDetails(index)}>More details</button>
+                            </div>
+                            <div className='hours-container hide'>
+                                {day?.hours?.map((hour: any, index: number) => (
+                                    <div key={index} className='single-hour-container'>
+                                        <p className='hour-display'>{displayHour(hour.datetime)}</p>
+                                        <img src={`src/assets/icon/${hour.icon}.svg`} alt={hour.icon} className='icon-display' />
+                                        <p className='temp-display'>{displayTemp(hour.temp)}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
